205383b355ae19bc439c3cf84d716a3ffca9e4cc
adaliszk <adaliszk@gmail.com>
2023-02-28 23:49:57 +0000

    WIP: fix the existing storybook configuration
    refs #77

--- packages/headless/.storybook/main.ts
+++ packages/headless/.storybook/main.ts
@@ -1,31 +1,35 @@
-import { qwikVite } from '@builder.io/qwik/optimizer';
-import { rootMain } from '../../../.storybook/main';
-
-import type { StorybookViteConfig } from '@storybook/builder-vite';
+import type { StorybookConfig } from 'storybook-framework-qwik';
+import type { UserConfig } from 'vite';
 
 import { mergeConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { qwikNxVite } from 'qwik-nx/plugins';
+import { qwikVite } from '@builder.io/qwik/optimizer';
 
-const config: StorybookViteConfig = {
-  ...rootMain,
+const config: StorybookConfig = {
   core: {
-    ...rootMain.core,
     builder: '@storybook/builder-vite',
+    disableTelemetry: true,
   },
+  framework: 'storybook-framework-qwik',
   stories: [
-    ...rootMain.stories,
-    '../src/lib/**/*.stories.mdx',
-    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/**/*.docs.mdx',
+    '../src/**/*.stories.tsx',
+    '../src/**/*.mdx',
   ],
-  addons: ['@storybook/addon-essentials', ...(rootMain.addons || [])],
-  framework: {
-    name: 'storybook-framework-qwik',
-  },
-  viteFinal: async (config: any) => {
+  addons: ['@storybook/addon-essentials'],
+  viteFinal: async (config: UserConfig) => {
     return mergeConfig(config, {
+      server: {
+        fs: {
+          // Allow serving files from the project root
+          allow: ['../../'],
+        },
+      },
       plugins: [
+        qwikNxVite(),
         viteTsConfigPaths({
-          root: '../../../',
+          projects: ['.storybook/tsconfig.json'],
         }),
         qwikVite(),
       ],
